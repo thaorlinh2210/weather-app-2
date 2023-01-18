766f5c1a0de765c8ff0dc728e71e662b96dc9e64
thaorlinh2210 <94463049+thaorlinh2210@users.noreply.github.com>
2023-01-19 01:39:25 +1100

    updated icon api

--- src/app.js
+++ src/app.js
@@ -20,6 +20,13 @@ function displayTemperature(response) {
 
   let dateElement = document.querySelector("#date");
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
+
+  let iconElement = document.querySelector("#icon");
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
 function formatDate(timestamp) {
